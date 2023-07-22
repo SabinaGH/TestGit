0a20ee77ab7664f46ff6b224ee88f3e0f1890df3
sabinag <sabinaghalan2021@gmail.com>
2023-07-21 22:19:43 -0500

    test

---
+++ TestPackageforGit/src/GitTest/CofeeShopOrderUsingSwitch.java
@@ -0,0 +1,47 @@
+package GitTest;
+
+import java.util.Scanner;
+
+public class CofeeShopOrderUsingSwitch {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Coffee Shop!");
+        System.out.println("Menu:");
+        System.out.println("1. Espresso"); //10$
+        System.out.println("2. Latte"); //15$
+        System.out.println("3. Cappuccino"); //5$
+        System.out.println("4. Americano"); //18$
+        System.out.print("Enter the number of your desired coffee: ");
+
+        int coffeeNumber = scanner.nextInt();
+
+        int totalPrice = 0;
+
+
+        switch(coffeeNumber)
+        {
+            case 1:
+                totalPrice = totalPrice+10;
+                break;
+            case 2:
+                totalPrice = totalPrice+15;
+                break;
+            case 3:
+                totalPrice = totalPrice+5;
+                break;
+            case 4:
+                totalPrice = totalPrice+18;
+                break;
+            default:
+                System.out.println("Please select valid coffee number");
+
+        }
+        // output as total price you have to pay to shopkeeper
+        System.out.println("Total price is = "+totalPrice);
+
+        scanner.close();
+    }
+}
